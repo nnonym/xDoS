05ac124b24671bcc7fd7841135dc7d52337d51e6
nnonym <58051143+nnonym@users.noreply.github.com>
2019-11-21 23:17:33 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+clear
+sudo apt update
+clear
+sudo apt install apache2
+clear
+chmod +x *
+sudo cp /$HOME/xDoS/xdos /usr/bin
+echo 'xDoS tool is succsessfull installed!'
+echo 'Now you can launch xDoS tool by command "xdos"!'
